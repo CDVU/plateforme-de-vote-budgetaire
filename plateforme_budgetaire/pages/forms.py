936113a6c89daacb5f3a2e9886574d1ff2a3d3a3
RignonNoel <rignon.noel@openmailbox.org>
2016-04-07 14:17:41 -0400

    add registration

--- plateforme_budgetaire/pages/forms.py
+++ plateforme_budgetaire/pages/forms.py
@@ -1,19 +1,26 @@
+# coding: utf-8
+
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import RegexValidator
+
+
+my_validator = RegexValidator(r"^[a-z\-]+\.[a-z\-]+.[0-9]+@ens.etsmtl.ca$", u"Votre email doit être du style prenom.nom.2@ens.etsmtl.ca.")
 
 
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['email']
+        fields = ['email', 'password']
 
     email = forms.CharField(
         label=_('Email'),
         required=True,
-        max_length=100
+        max_length=100,
+        validators=[my_validator]
     )
-    email.widget.attrs.update({'placeholder': _(u'AB12345@ens.etsmtl.ca'), 'class': 'form-control'})
+    email.widget.attrs.update({'placeholder': _(u'prenom.nom.2@ens.etsmtl.ca'), 'class': 'form-control'})
 
     password = forms.CharField(
         label=_('Password'),
--- plateforme_budgetaire/pages/models.py
+++ plateforme_budgetaire/pages/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+# coding: utf-8
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Hash(models.Model):
+    class Meta:
+        verbose_name_plural = 'Hashs'
+
+
+    user = models.ForeignKey(
+        User,
+        verbose_name='User',
+        related_name='Hashs'
+    )
+
+    hash = models.TextField(
+        verbose_name='hash'
+    )
+
+    duration = models.DurationField(
+        verbose_name='Temps de vie'
+    )
+
+    action = models.TextField(
+        verbose_name='Action authorisé'
+    )
+
+    created = models.DateTimeField(
+        verbose_name='Date of creation'
+    )
+
+    used = models.BooleanField(
+        verbose_name='Utilisé',
+        default=False
+    )
+
+    def __str__(self):
+        return str(self.user) + ' - ' + str(self.action) + ' - ' + str(self.created)
+
+    def save(self, *args, **kwarg):
+        if not self.id:
+            self.created = timezone.now()
+
+        super(Hash, self).save(*args, **kwarg)
