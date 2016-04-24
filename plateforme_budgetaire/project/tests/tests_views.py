a2b6e6d96be8e060c54a75434716923fa28e6a13
RignonNoel <rignon.noel@openmailbox.org>
2016-04-24 08:28:25 -0400

    add test on project create

--- plateforme_budgetaire/project/tests/tests_views.py
+++ plateforme_budgetaire/project/tests/tests_views.py
@@ -5,6 +5,7 @@ from project.factories import ProjectFactory, SubProjectFactory
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.core.urlresolvers import reverse
+import datetime
 
 
 class ProjectListViewTests(TestCase):
@@ -176,3 +177,102 @@ class ProjectDetailViewTests(TestCase):
             follow=False
         )
         self.assertEqual(result.status_code, 302)
+
+
+class ProjectCreateViewTests(TestCase):
+
+    def setUp(self):
+        settings.EMAIL_BACKEND = \
+            'django.core.mail.backends.locmem.EmailBackend'
+
+        self.user = User.objects.create_user(
+            username='am56680@ens.etsmtl.ca',
+            email='am56680@ens.etsmtl.ca',
+            password='passUser'
+        )
+        self.admin = User.objects.create_superuser(
+            username='admin',
+            email='rignon.noel@openmailbox.org',
+            password='passAdmin'
+        )
+
+    def test_access_as_user(self):
+        self.client.logout()
+        self.client.login(
+            username=self.user.username,
+            password="passUser"
+        )
+
+        result = self.client.get(
+            reverse('projects:project_create'),
+            follow=False
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_access_as_admin(self):
+        self.client.logout()
+        self.client.login(
+            username=self.admin.username,
+            password="passAdmin"
+        )
+
+        result = self.client.get(
+            reverse('projects:project_create'),
+            follow=False
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_access_as_logout(self):
+        self.client.logout()
+
+        result = self.client.get(
+            reverse('projects:project_create'),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 302)
+
+    def test_add_a_project(self):
+        self.client.logout()
+        self.client.login(
+            username=self.user.username,
+            password="passUser"
+        )
+
+        data = {
+            'name': 'Nouveau projet',
+            'description': 'Ma description',
+            'number_affected_by': '345',
+            'justification': 'Ma justification',
+            'completion_time': datetime.timedelta(1157, 35200),
+            'date_of_submission': datetime.datetime(2016, 4, 1, 14, 41, 39),
+            'author_name': 'Noël Rignon',
+            'author_website': 'http://RignonNoel.github.io',
+            'author_description': 'Étudiant génie logiciel',
+        }
+
+        result = self.client.post(
+            reverse('projects:project_create'),
+            data,
+            follow=False
+        )
+        self.assertEqual(result.status_code, 302)
+
+    def test_missing_fields_when_add_a_project(self):
+        self.client.logout()
+        self.client.login(
+            username=self.user.username,
+            password="passUser"
+        )
+
+        data = {
+            'name': 'Nouveau projet'
+        }
+
+        result = self.client.post(
+            reverse('projects:project_create'),
+            data,
+            follow=False
+        )
+        self.assertEqual(result.status_code, 200)
