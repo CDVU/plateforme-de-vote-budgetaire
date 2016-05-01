b6e54c86bf3c96a05df093c166efb32df3925cb3
RignonNoel <rignon.noel@openmailbox.org>
2016-05-01 18:52:25 -0400

    select form by id

--- plateforme_budgetaire/vote/urls.py
+++ plateforme_budgetaire/vote/urls.py
@@ -4,5 +4,5 @@ from vote.views import *
 urlpatterns = patterns(
     '',
     url(r'^$', Home.as_view(), name='home'),
-    url(r'^form/$', Form.as_view(), name='form'),
+    url(r'^form/(?P<pk>\d+)$', Form.as_view(), name='form'),
 )
