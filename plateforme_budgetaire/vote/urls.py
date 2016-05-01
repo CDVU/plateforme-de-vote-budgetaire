5a3eed180a3f260099412cbbd11c7c3010aab8e7
RignonNoel <rignon.noel@openmailbox.org>
2016-05-01 18:30:57 -0400

    add templateView to list vote

--- plateforme_budgetaire/vote/urls.py
+++ plateforme_budgetaire/vote/urls.py
@@ -3,5 +3,6 @@ from vote.views import *
 
 urlpatterns = patterns(
     '',
-    url(r'^$', Form.as_view(), name='form'),
+    url(r'^$', Home.as_view(), name='home'),
+    url(r'^form/$', Form.as_view(), name='form'),
 )
